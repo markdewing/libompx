ea59e3c9269e1c3c9f49ac0e22ae8394ad5d6a85
Mark Dewing <markdewing@gmail.com>
2023-05-08 18:54:05 -0500

    Pass start pointer and number of elements

--- lib/oneapi/sort_impl.cpp
+++ lib/oneapi/sort_impl.cpp
@@ -8,11 +8,11 @@
 namespace ompx {
 namespace device {
 
-template <typename T> void ompx_sort(T B, T E) {
-  std::sort(oneapi::dpl::execution::dpcpp_default, B, E);
+template <typename T> void ompx_sort(T B, size_t NumElements) {
+  std::sort(oneapi::dpl::execution::dpcpp_default, B, B+NumElements);
 }
 
-template void ompx_sort(int *, int *);
+template void ompx_sort(int *, size_t);
 
 } // namespace device
 } // namespace ompx
