8826b6c4525899f5b361ac0640e744d141b2dbb1
Mark Dewing <markdewing@gmail.com>
2023-05-12 16:08:45 -0500

    Add check if data is present on device
    If not present, transfer it.

--- include/ompx/sort.h
+++ include/ompx/sort.h
@@ -1,6 +1,7 @@
 #ifndef OMPX_SORT_H
 #define OMPX_SORT_H
 
+#include <omp.h>
 #include <stddef.h>
 
 namespace ompx {
@@ -23,8 +24,16 @@ void ompx_sort(RandomAccessIterator first, size_t NumElements);
 template <typename RandomAccessIterator>
 void sort(RandomAccessIterator first, RandomAccessIterator last) {
   size_t NumElements = last - first;
+  int dev = omp_get_default_device();
+  int present = omp_target_is_present(first, dev);
+  if (present) {
 #pragma omp target data use_device_ptr(first)
-  ompx::device::ompx_sort(first, NumElements);
+    ompx::device::ompx_sort(first, NumElements);
+  } else {
+#pragma omp target data map(tofrom : first[ : NumElements])                    \
+    use_device_ptr(first)
+    ompx::device::ompx_sort(first, NumElements);
+  }
 }
 } // namespace device
 } // namespace ompx
