fc39534097d2be15c3de0231c52012feae6826de
Mark Dewing <markdewing@gmail.com>
2023-05-17 17:21:17 -0500

    Change oneapi implementation

--- include/ompx/sort.h
+++ include/ompx/sort.h
@@ -3,6 +3,10 @@
 
 #include <omp.h>
 #include <stddef.h>
+#ifdef ONEAPI
+#include <oneapi/dpl/algorithm>
+#include <oneapi/dpl/execution>
+#endif
 
 namespace ompx {
 
@@ -35,6 +39,40 @@ template <typename T> void sort(T *first, T *last, ompx_sort_cmp_ty Cmp) {
 //
 
 namespace device {
+#ifdef ONEAPI
+template <typename T> void sort(T *first, T *last) {
+  size_t NumElements = last - first;
+  int dev = omp_get_default_device();
+  int present = omp_target_is_present(first, dev);
+  if (present) {
+#pragma omp target data use_device_ptr(first)
+    std::sort(oneapi::dpl::execution::dpcpp_default, first,
+              first + NumElements);
+  } else {
+#pragma omp target data map(tofrom : first[ : NumElements])                    \
+    use_device_ptr(first)
+    std::sort(oneapi::dpl::execution::dpcpp_default, first,
+              first + NumElements);
+  }
+}
+
+template <typename T, typename Cmp> void sort(T *first, T *last, Cmp F) {
+  size_t NumElements = last - first;
+  int dev = omp_get_default_device();
+  int present = omp_target_is_present(first, dev);
+  if (present) {
+#pragma omp target data use_device_ptr(first)
+    std::sort(oneapi::dpl::execution::dpcpp_default, first, first + NumElements,
+              F);
+  } else {
+#pragma omp target data map(tofrom : first[ : NumElements])                    \
+    use_device_ptr(first)
+    std::sort(oneapi::dpl::execution::dpcpp_default, first, first + NumElements,
+              F);
+  }
+}
+
+#else
 template <typename T> void ompx_sort(T *first, size_t NumElements);
 
 void ompx_sort_cmp(void *first, size_t NumElements, size_t elementSize,
@@ -67,6 +105,8 @@ template <typename T> void sort(T *first, T *last, ompx_sort_cmp_ty Cmp) {
     ompx::device::ompx_sort_cmp(first, NumElements, sizeof(T), Cmp);
   }
 }
+#endif
+
 } // namespace device
 } // namespace ompx
 
--- lib/oneapi/sort_impl.cpp
+++ lib/oneapi/sort_impl.cpp
@@ -5,14 +5,3 @@
 #include <oneapi/dpl/algorithm>
 #include <oneapi/dpl/execution>
 
-namespace ompx {
-namespace device {
-
-template <typename T> void ompx_sort(T* B, size_t NumElements) {
-  std::sort(oneapi::dpl::execution::dpcpp_default, B, B+NumElements);
-}
-
-#include "../inst/sort_inst.h"
-
-} // namespace device
-} // namespace ompx
