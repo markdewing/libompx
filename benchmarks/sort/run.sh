9149320a0ed4173d830011c6a17581e9c3f53b08
Mark Dewing <markdewing@gmail.com>
2023-08-31 11:56:55 -0500

    Add sort performance test

---
+++ benchmarks/sort/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+EXE=./ompx/a.out
+${EXE} --only-print-header
+for n in 1000 2000 5000 10000 20000 50000 100000 200000 500000 1000000 2000000
+do
+    ${EXE} -n $n --no-header -l 100
+done
